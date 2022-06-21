e6b70ebeaf7c9c0f2b2880f611ec5f26da12dc80
jaydip216 <jaydip.bhanderi216@gmail.com>
2022-06-21 10:12:06 +0530

    Linked list

---
+++ src/com/linkedList/LinkedListCycle2.java
@@ -0,0 +1,31 @@
+package com.linkedList;
+
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+public class LinkedListCycle2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode detectCycle(ListNode head) {
+		ListNode fast = head;
+		ListNode slow = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast)
+				break;
+		}
+		if (fast == null || fast.next == null) {
+			return null;
+		}
+		while (head != slow) {
+			head = head.next;
+			slow = slow.next;
+		}
+		return head;
+	}
+}
---
+++ src/com/linkedList/RemoveDuplicatedFromList2.java
@@ -0,0 +1,42 @@
+package com.linkedList;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+
+public class RemoveDuplicatedFromList2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode prev = null;
+		ListNode temp = head;
+
+		if (head == null)
+			return head;
+
+		if (head.next == null)
+			return head;
+		int count = 0;
+		while (temp != null && temp.next != null) {
+			while (temp != null && temp.next != null && temp.val == temp.next.val) {
+				temp.next = temp.next.next;
+				count += 1;
+			}
+			if (count != 0) {
+				if (prev == null) {
+					head = temp.next;
+				} else {
+					prev.next = temp.next;
+				}
+				temp = temp.next;
+				count = 0;
+			} else {
+				prev = temp;
+				temp = temp.next;
+			}
+		}
+		return head;
+	}
+}
